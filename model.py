36f83b08f45dea6271f64f9428307aac182a0512
Naman Raiyani <namanraiyani@gmail.com>
2025-01-12 23:17:54 +0530

    Just initializing

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import math
 import torch
 import torch.nn as nn
 
@@ -6,4 +7,25 @@ class InputEmbeddings(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, d_model:int, seq_len:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
         
+        pe = torch.zeros(seq_len, d_model) #Postional Encoding
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # Vector shaped (seq_len, 1)
+        division_term = torch.exp(torch.arange(0, d_model, 2).float()*(-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position*division_term) # Apply sin to even positions
+        pe[:, 1::2] = torch.cos(position*division_term) # Apply cos to odd positions
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe',pe)
+        
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires._grad_(False)
+        return self.dropout(x)
